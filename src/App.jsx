cf5af82f69a08f2104aaecea0f2c96f87fd6f094
gnanachandra <vvit.20bq1a05p2@gmail.com>
2023-12-22 20:42:26 +0530

    project setup - initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-screen h-screen text-2xl font-medium flex items-center justify-center flex-col">
+      BigSteps Task by V.Gnana Chandra
+    </div>
+  );
+};
+
+export default App;
