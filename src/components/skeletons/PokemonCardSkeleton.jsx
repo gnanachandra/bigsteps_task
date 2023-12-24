faee25fcacbe7854cb0c04b94fe0d867d3eb6f01
gnanachandra <vvit.20bq1a05p2@gmail.com>
2023-12-24 10:26:57 +0530

    show pokemon data and loading skeleton in home page

---
+++ src/components/skeletons/PokemonCardSkeleton.jsx
@@ -0,0 +1,33 @@
+import { Card, CardBody, CardHeader } from "@material-tailwind/react";
+
+const PokemonCardSkeleton = () => {
+  return (
+    <Card className="rounded-md mx-auto  w-[300px] lg:w-full h-full shadow-sm border border-gray-300 hover:shadow-md hover:scale-105 transition-all duration-1000 cursor-pointer ease-in-out p-3 animate-pulse sapce-y-1.5">
+      <CardHeader
+        floated={false}
+        shadow={false}
+        className="capitalize w-14 h-3 bg-gray-300 rounded"
+      />
+
+      <CardBody>
+        <div className="flex justify-between">
+          {/* Types */}
+          <div className="flex flex-col gap-2">
+            {Array.from([1, 2]).map((type, i) => {
+              return (
+                <div
+                  className="w-20 h-3 shadow-none bg-gray-300 rounded"
+                  key={i}
+                />
+              );
+            })}
+          </div>
+          {/* Image */}
+          <div className="w-20 h-20 rounded-full shadow-none bg-gray-300"></div>
+        </div>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default PokemonCardSkeleton;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,30 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPokemonData } from "../redux/pokemon/pokemonSlice";
 import PokemonCard from "../components/PokemonCard";
+import PokemonCardSkeleton from "../components/skeletons/PokemonCardSkeleton";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { pokemons, isLoading } = useSelector((store) => store["pokemon"]);
+  const { pokemons } = useSelector((store) => store["pokemon"]);
   useEffect(() => {
     const fetchPokemonData = async () => {
       await dispatch(getPokemonData());
     };
     fetchPokemonData();
   }, []);
-  if (isLoading) {
-    return <p>Loading</p>;
-  }
+
   return (
     <div className="max-w-7xl mx-auto my-10">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 items-center justify-center mx-auto w-full h-full">
-        {pokemons?.map((item, index) => {
-          return <PokemonCard pokemon={item} key={index} index={index + 1} />;
-        })}
+        {pokemons.length > 0
+          ? pokemons?.map((item, index) => {
+              return (
+                <PokemonCard pokemon={item} key={index} index={index + 1} />
+              );
+            })
+          : Array.from({ length: 12 }, (_, index) => (
+              <PokemonCardSkeleton key={index} />
+            ))}
       </div>
     </div>
   );
